94fcfd82143a0b737766953ad0eac0ea02bb8307
isseiarai <zm19416@josai.ac.jp>
2021-12-07 01:59:12 +0000

    12/07

--- ex06-10.js
+++ ex06-10.js
@@ -2,6 +2,9 @@ function imgchange1() {
     document.getElementById('logo').src=
      './image/1-61.jpg';
     }
-   
+    function imgchange2() {
+        document.getElementById('logo').src=
+         './image/keiei_small.jpg';
+        }
 
     
